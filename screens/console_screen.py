5f9944b1c4c7299bf0fb6681147b67f7e00f9a1c
Trashytalk <trashemaillll123@gmail.com>
2025-06-12 19:40:28 -0400

    Update console_screen.py

--- screens/console_screen.py
+++ screens/console_screen.py
@@ -2,8 +2,13 @@
 
 from kivy.uix.screenmanager import Screen
 
+from widgets import LogViewer
+
 
 class ConsoleScreen(Screen):
-    """Placeholder for a scrolling console output widget."""
+    """Display real-time logs from Kismet or other services."""
 
-    # Implementation will be added in the future
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.log_widget = LogViewer()
+        self.add_widget(self.log_widget)
