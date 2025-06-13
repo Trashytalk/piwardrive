56089a5bce6e0644ee0367fa03aa8ce915ad98f3
Trashytalk <trashemaillll123@gmail.com>
2025-06-13 16:52:25 -0400

    Update console_screen.py

--- screens/console_screen.py
+++ screens/console_screen.py
@@ -1,14 +1,15 @@
-"""Simple screen hosting a console log view."""
-
-from kivy.uix.screenmanager import Screen
-
-from widgets import LogViewer
-
-
-class ConsoleScreen(Screen):
-    """Display real-time logs from Kismet or other services."""
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.log_widget = LogViewer()
-        self.add_widget(self.log_widget)
++14-14
+"""Simple screen hosting a console log view."""␊
+␊
+from kivy.uix.screenmanager import Screen␊
+␊
+from widgets import LogViewer␊
+␊
+␊
+class ConsoleScreen(Screen):␊
+    """Display real-time logs from Kismet or other services."""␊
+␊
+    def __init__(self, **kwargs):␊
+        super().__init__(**kwargs)␊
+        self.log_widget = LogViewer()␊
+        self.add_widget(self.log_widget)␊
