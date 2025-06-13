74cefe59074ed3d5bb4d76addf550a93d3a5c081
Trashytalk <trashemaillll123@gmail.com>
2025-06-12 20:50:16 -0400

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,8 @@ import tempfile
 from unittest import mock
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.modules.setdefault('psutil', mock.Mock())
+sys.modules.setdefault('requests', mock.Mock())
 import utils
 
 
