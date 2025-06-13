edb038baeac19b06975f5c8680fd8fc3d34d358b
Trashytalk <trashemaillll123@gmail.com>
2025-06-12 21:36:57 -0400

    Update __init__.py

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,5 +1,19 @@
 """Custom widget package for PiWardrive."""
 
-from .log_viewer import LogViewer
 
-__all__ = ["LogViewer"]
+
+from .log_viewer import LogViewer
+from .signal_strength import SignalStrengthWidget
+from .gps_status import GPSStatusWidget
+from .handshake_counter import HandshakeCounterWidget
+from .service_status import ServiceStatusWidget
+from .storage_usage import StorageUsageWidget
+
+__all__ = [
+    "LogViewer",
+    "SignalStrengthWidget",
+    "GPSStatusWidget",
+    "HandshakeCounterWidget",
+    "ServiceStatusWidget",
+    "StorageUsageWidget",
+]
