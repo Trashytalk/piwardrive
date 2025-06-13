89009dec9f2380c62ba0951295dbce52bb6c4139
Trashytalk <trashemaillll123@gmail.com>
2025-06-13 17:57:22 -0400

    Update base.py

--- widgets/base.py
+++ widgets/base.py
@@ -1,4 +1,7 @@
++4-2
 """Base classes for dashboard widgets with drag-and-drop support."""
+
+from typing import Any
 
 from kivy.uix.behaviors import DragBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -9,11 +12,11 @@ class DashboardWidget(DragBehavior, MDBoxLayout):
 
     update_interval = 5.0
     
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
         self.drag_rectangle = self.x, self.y, self.width, self.height
         self.drag_timeout = 10000000
         self.drag_distance = 0
 
-    def on_size(self, *args):
+    def on_size(self, *args: Any) -> None:
         self.drag_rectangle = self.x, self.y, self.width, self.height
