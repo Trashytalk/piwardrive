2dcf8dd7dd54b9a6470169a7f2f8df34f412a2b3
Trashytalk <trashemaillll123@gmail.com>
2025-06-12 23:42:09 -0400

    Update __init__.py

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -8,6 +8,9 @@ from .gps_status import GPSStatusWidget
 from .handshake_counter import HandshakeCounterWidget
 from .service_status import ServiceStatusWidget
 from .storage_usage import StorageUsageWidget
+from .disk_trend import DiskUsageTrendWidget
+from .cpu_temp_graph import CPUTempGraphWidget
+from .net_throughput import NetworkThroughputWidget
 
 __all__ = [
     "LogViewer",
@@ -16,4 +19,7 @@ __all__ = [
     "HandshakeCounterWidget",
     "ServiceStatusWidget",
     "StorageUsageWidget",
+    "DiskUsageTrendWidget",
+    "CPUTempGraphWidget",
+    "NetworkThroughputWidget",
 ]
