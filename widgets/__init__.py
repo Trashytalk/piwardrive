9690748c9a8ca7cafb5733e08e71da72a5d8fe92
Trashytalk <trashemaillll123@gmail.com>
2025-06-13 18:41:38 -0400

    Update __init__.py

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,25 +1,26 @@
-"""Custom widget package for PiWardrive."""␊
-␊
-␊
-␊
-from .log_viewer import LogViewer␊
-from .signal_strength import SignalStrengthWidget␊
-from .gps_status import GPSStatusWidget␊
-from .handshake_counter import HandshakeCounterWidget␊
-from .service_status import ServiceStatusWidget␊
-from .storage_usage import StorageUsageWidget␊
-from .disk_trend import DiskUsageTrendWidget␊
-from .cpu_temp_graph import CPUTempGraphWidget␊
-from .net_throughput import NetworkThroughputWidget␊
-␊
-__all__ = [␊
-    "LogViewer",␊
-    "SignalStrengthWidget",␊
-    "GPSStatusWidget",␊
-    "HandshakeCounterWidget",␊
-    "ServiceStatusWidget",␊
-    "StorageUsageWidget",␊
-    "DiskUsageTrendWidget",␊
-    "CPUTempGraphWidget",␊
-    "NetworkThroughputWidget",␊
-]␊
+"""Custom widget package for PiWardrive."""
+
+
+from .log_viewer import LogViewer
+from .signal_strength import SignalStrengthWidget
+from .gps_status import GPSStatusWidget
+from .handshake_counter import HandshakeCounterWidget
+from .service_status import ServiceStatusWidget
+from .storage_usage import StorageUsageWidget
+from .disk_trend import DiskUsageTrendWidget
+from .cpu_temp_graph import CPUTempGraphWidget
+from .net_throughput import NetworkThroughputWidget
+
+
+
+__all__ = [
+    "LogViewer",
+    "SignalStrengthWidget",
+    "GPSStatusWidget",
+    "HandshakeCounterWidget",
+    "ServiceStatusWidget",
+    "StorageUsageWidget",
+    "DiskUsageTrendWidget",
+    "CPUTempGraphWidget",
+    "NetworkThroughputWidget",
+]
