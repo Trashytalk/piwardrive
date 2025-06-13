2e8eea7ef367ef43b8dc66b5c8d4df0aea0e4ea0
Trashytalk <trashemaillll123@gmail.com>
2025-06-13 17:59:45 -0400

    Update service_status.py

--- widgets/service_status.py
+++ widgets/service_status.py
@@ -1,6 +1,7 @@
 """Widget showing whether services are running."""
 
 import logging
+from typing import Any
 from kivymd.uix.label import MDLabel
 
 from .base import DashboardWidget
@@ -9,13 +10,14 @@ from utils import service_status
 
 class ServiceStatusWidget(DashboardWidget):
     update_interval = 5.0
-    def __init__(self, **kwargs):
+
+    def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
         self.label = MDLabel(text="Services: N/A")
         self.add_widget(self.label)
         self.update()
 
-    def update(self):
+    def update(self) -> None:
         try:
             kis = service_status('kismet')
             btc = service_status('bettercap')
