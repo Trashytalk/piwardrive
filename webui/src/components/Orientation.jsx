3fe10b851cc6e61332e867f7b76c16f4326f26a6
Trashytalk <trashemaillll123@gmail.com>
2025-06-24 21:43:18 -0400

    feat(webui): poll orientation and vehicle info

--- webui/src/components/Orientation.jsx
+++ webui/src/components/Orientation.jsx
@@ -4,12 +4,21 @@ export default function Orientation({ data }) {
   const [info, setInfo] = useState(data);
 
   useEffect(() => {
-    if (!data) {
+    if (data) {
+      setInfo(data);
+      return;
+    }
+
+    const load = () => {
       fetch('/orientation')
         .then(r => r.json())
         .then(setInfo)
         .catch(() => setInfo(null));
-    }
+    };
+
+    load();
+    const id = setInterval(load, 5000);
+    return () => clearInterval(id);
   }, [data]);
 
   if (!info) return <div>Orientation: N/A</div>;
