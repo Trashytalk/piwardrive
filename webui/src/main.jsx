7317bbced2387869b5562bbda0bffc915af50ea3
Trashytalk <trashemaillll123@gmail.com>
2025-06-24 21:45:48 -0400

    Add SplitScreen React component

--- webui/src/main.jsx
+++ webui/src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import ConsoleView from './components/ConsoleView.jsx';
-import SplitView from './components/SplitView.jsx';
+import SplitScreen from './components/SplitScreen.jsx';
 
 let Root = App;
 const path = window.location.pathname;
 if (path.startsWith('/console')) Root = ConsoleView;
-if (path.startsWith('/split')) Root = SplitView;
+if (path.startsWith('/split')) Root = SplitScreen;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
