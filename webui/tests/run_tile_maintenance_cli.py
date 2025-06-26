525f78bbcc26afac8fc4d5c2c76c8dbc03da1d48
Trashytalk <trashemaillll123@gmail.com>
2025-06-26 18:02:24 -0400

    Add Node wrapper and test for tile maintenance CLI

---
+++ webui/tests/run_tile_maintenance_cli.py
@@ -0,0 +1,23 @@
+import sys
+import json
+
+from piwardrive.scripts import tile_maintenance_cli as cli
+
+called = {}
+
+def fake_purge(folder, days):
+    called["purge"] = [folder, int(days)]
+
+def fake_limit(folder, limit):
+    called["limit"] = [folder, int(limit)]
+
+def fake_vacuum(path):
+    called["vacuum"] = path
+
+cli.tile_maintenance.purge_old_tiles = fake_purge
+cli.tile_maintenance.enforce_cache_limit = fake_limit
+cli.tile_maintenance.vacuum_mbtiles = fake_vacuum
+
+cli.main(sys.argv[1:])
+
+print(json.dumps(called))
