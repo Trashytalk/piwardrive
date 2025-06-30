8af27c7e0d692ed934c53cc972f3f3db4565aa0c
Trashytalk <trashemaillll123@gmail.com>
2025-06-30 17:47:16 -0400

    Restrict sync receiver host

--- sync_receiver.py
+++ sync_receiver.py
@@ -22,4 +22,4 @@ async def receive(file: UploadFile):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="127.0.0.1", port=9000)
